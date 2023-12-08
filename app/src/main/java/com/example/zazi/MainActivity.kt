bc4fd83350b1d0133db8f5068a002a866c33f3bb
d1sold <derman.sav@yandex.ru>
2023-12-08 19:04:17 +0500

    Initial commit

---
+++ app/src/main/java/com/example/zazi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zazi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a333.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
